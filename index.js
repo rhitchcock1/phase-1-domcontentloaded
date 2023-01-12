6a5d6cf20da5c84b6c309ef5dc375ed831c0c1c2
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-01-12 15:25:32 -0500

    DOM Edit Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   newText();
+  });
+  function newText(){
+    document.getElementById( "text" )
+    .textContent = "This is really cool!";
+  }
